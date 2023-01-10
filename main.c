eebc26868d0384a0eee4a9f8fc8b0b41c0dcc293
zerocool <46464404+z3roco01@users.noreply.github.com>
2023-01-10 09:31:04 -0600

    Fix arg name on tokenize

--- main.c
+++ main.c
@@ -69,104 +69,7 @@ void printTok(tok_t tok) {
 	}
 }
 
-/*uint32_t tokenize(char* code, tok_t* toks) {
-	uint32_t tokCount    = 0;
-	uint8_t  gotLineNum  = 0;
-	uint32_t curLineNum  = 0;
-	uint32_t gettingArgs = 0;
-
-	for(uint32_t i = 0; i < strlen(code); ++i) {
-		if(!gotLineNum && !gettingArgs) {
-				char* numS = calloc(1, MAX_NUM_DIGITS+1);
-				uint32_t j = i;
-				while(code[j] >= '0' && code[j] <= '9' && j-i < MAX_NUM_DIGITS && code[j] != ' ') {
-					if(code[j] > '9' && code[j] < '0'){
-						printf("NOT A VALID LINE NUMBER");
-						free(numS);
-						return 1;
-					}
-					numS[j-i] = code[j];
-					j++;
-				}
-				i = j;
-				curLineNum = atoi(numS);
-				gotLineNum = 1;
-		}else {
-			if(code[i] == ' ' || code[i] == '\t' || code[i] == '\n' || code[i] == '\0') {
-
-			}else if(code[i] == '"') {
-				// Parsing a string
-
-				char* str = calloc(1, MAX_STR_LEN+1);
-				i++;
-				uint32_t j = i;
-				while(code[j] != '"' && j-i < MAX_STR_LEN) {
-					str[j-i] = code[j];
-					j++;
-				}
-
-				toks[tokCount].type   = STR;
-				toks[tokCount++].data = str;
-
-				i = j;
-			}else if(code[i] <= '9' && code[i] >= '0') {
-				// Parsing a number
-
-				char* numS = calloc(1, MAX_NUM_DIGITS+1);
-				uint32_t j = i;
-				while(code[j] >= '0' && code[j] <= '9' && j-i < MAX_NUM_DIGITS) {
-					numS[j-i] = code[j];
-					j++;
-				}
-
-				int32_t* num = malloc(sizeof(int32_t));
-				*num = atoi(numS);
-
-				toks[tokCount].type   = NUM;
-				toks[tokCount++].data = num;
-
-				i = j;
-			}else {
-				// Parsing a symbol
-
-				char* symbol = calloc(1, MAX_SYM_LEN);
-				uint32_t j = i;
-				while(code[j] != ' ' && code[j] != '(' && code[j] != '\0' && j-i < MAX_SYM_LEN-1) {
-					symbol[j-i] = code[j];
-					j++;
-				}
-
-				uint32_t k;
-				uint8_t match = 0;
-				for(k = 0; k < SYMS_LEN; ++k) {
-					if(strcmp(symbol, SYMBOLS[k].name) == 0) {
-						match = 1;
-						gettingArgs = SYMBOLS[k].argCount;
-						break;
-					}
-				}
-				if(match == 0) {
-					printf("UNKNOWN SYMBOL: %s\n", symbol);
-					return 0;
-				}
-
-				toks[tokCount].type   = SYM;
-				symbols_t symNum = k;
-				toks[tokCount++].data = &symNum;
-
-				i = j;
-			}
-
-			toks[tokCount-1].lineNum = curLineNum;
-			gotLineNum = 0;
-		}
-	}
-
-	toks[tokCount++].type = END;
-	return tokCount;
-}*/
-
-uint32_t newTokenize(line_t* lines, char* code, uint32_t codeLen) {
+uint32_t tokenize(line_t* lines, char* code, uint32_t codeLen) {
 	uint8_t  gotLineNum = 0;
 	uint32_t lineInd    = 0;
 	uint32_t curLineNum = 0;
@@ -320,43 +223,7 @@ uint8_t basicPrint(tok_t arg) {
 	return 0;
 }
 
-// returns 0 on success anything else is failure
-/*uint8_t interpret(tok_t* toks, uint32_t tokCount) {
-	for(uint32_t i = 0; i < tokCount; ++i) {
-		switch(toks[i].type) {
-			case NUM:
-				break;
-			case STR:
-				break;
-			case SYM:
-				switch(*(int32_t*)toks[i].data) {
-					case PRINT:
-						if(toks[i+1].lineNum == toks[i].lineNum && basicPrint(toks[i+1])) {
-							printf("INVALID ARGUMENT TYPE OF %i FOR %s\n", toks[i].type, SYMBOLS[PRINT]);
-						}
-						i++;
-						break;
-					case GOTO:
-						break;
-					default:
-						printf("UNKONW SYMBOL WITH NUMBER: %i", *(int32_t*)toks[i].data);
-						break;
-				}
-				break;
-			case END:
-				return 0;
-				break;
-			default:
-				printf("UNKOWN TOKEN: %i", toks[i].type);
-				return 1;
-				break;
-		}
-	}
-
-	return 0;
-}*/
-
-uint8_t newInterpret(line_t* lines, uint32_t lineCnt) {
+uint8_t interpret(line_t* lines, uint32_t lineCnt) {
 	tok_t* curTok;
 	for(uint32_t i = 0; i < lineCnt; ++i) {
 		printf("%u\n", lines[i].num);
@@ -406,21 +273,7 @@ int main(void){
 
 	line_t* lines = malloc(MAX_BASIC_LINES * sizeof(line_t));
 
-	uint32_t lineCnt = newTokenize(lines, code, strlen(code));
+	uint32_t lineCnt = tokenize(lines, code, strlen(code));
 
-	return newInterpret(lines, lineCnt);
-	/*tok_t* toks;
-	toks = malloc(MAX_BASIC_TOKS * sizeof(tok_t));
-	uint32_t tokCount = tokenize(code, toks);
-	//char* line = calloc(1, MAX_STR_LEN+1);
-	while(1){
-		fgets(line, MAX_STR_LEN, stdin);
-		uint32_t tokCount = tokenize(line, toks);
-		uint8_t result = interpret(toks, tokCount);
-	}
-	for(uint32_t i = 0; i < tokCount; ++i)
-		printTok(toks[i]);
-
-	return interpret(toks, tokCount);*/
+	return interpret(lines, lineCnt);
 }
-
